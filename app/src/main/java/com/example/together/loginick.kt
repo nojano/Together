2281da224da318205530252a5335e0635cc916b7
Nickdiesse <101928091+Nickdiesse@users.noreply.github.com>
2022-05-11 18:52:17 +0200

    login di nick

--- app/src/main/java/com/example/together/loginick.kt
+++ app/src/main/java/com/example/together/loginick.kt
@@ -3,11 +3,20 @@ package com.example.together
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class loginick : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_loginick)
+
+
     }
 
 
 }
+
+
+
+
+
+
+
