6bbc35b87cb886ff583d4804f9de271caec2ff02
Alexeon10 <alessandro.appolloni21@gmail.com>
2022-05-13 17:51:16 +0200

    login page Ale

---
+++ app/src/main/java/com/example/together/loginAle.kt
@@ -0,0 +1,15 @@
+package com.example.together
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginAle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_ale)
+
+
+    }
+
+
+}
