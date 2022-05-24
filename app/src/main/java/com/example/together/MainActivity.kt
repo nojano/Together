840cc5eece37f5da5bfe2cf1ee300cd12ecab561
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-05-24 18:48:43 +0200

    Login button link working

--- app/src/main/java/com/example/together/MainActivity.kt
+++ app/src/main/java/com/example/together/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.together
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val button:Button = findViewById(R.id.loginbtn)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewPostActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
