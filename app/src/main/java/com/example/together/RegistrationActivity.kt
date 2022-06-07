93a1e82212e719ca9a2dfece31ad2eaf55eeb1eb
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-06-08 00:13:16 +0200

    -settings.gradle changed! Impossible to continue otherwise -RegistrationActivity.kt validate password and register user on firebase without checking for existence -gradle.properties removed unused property android.enableJetifier -Added google-services.json -configured build.gradle (app level and proj level) for firebase -activity_registration.xml renamed id for signup, conflict with activity_login.xml @signupbtn

--- app/src/main/java/com/example/together/RegistrationActivity.kt
+++ app/src/main/java/com/example/together/RegistrationActivity.kt
@@ -1,14 +1,81 @@
 package com.example.together
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        val signupButton: Button = findViewById(R.id.signupbtndone)
+        signupButton.setOnClickListener {
+
+            //Validate input from user
+            if (passwordCheck(
+                    this@RegistrationActivity,
+                    findViewById(R.id.password),
+                    findViewById(R.id.repeatpassword)
+                )
+            ) {
+
+                //prepare the user instance
+                val user = getUserAttribute(this@RegistrationActivity)
+
+                val TAG = "RegistrationActivity"
+                val db = Firebase.firestore
+                // Add a new document with a generated ID
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
+                /*
+                //Registration Successful, redirect to HomepageActivity
+                val intent = Intent(
+                    this@RegistrationActivity,
+                    HomepageActivity::class.java
+                )  //switch to another activity
+                startActivity(intent)
+                */
+            }
+        }
     }
+}
+
+fun passwordCheck(
+    activity: RegistrationActivity,
+    password: EditText,
+    repeatPsw: EditText
+): Boolean {
+    if (password.text.toString() != repeatPsw.text.toString()) {
+        Toast.makeText(activity, "The passwords are not equals", Toast.LENGTH_SHORT).show()
+        return false
+    }
+    if (password.text.toString().length < 8) {
+        Toast.makeText(activity, "Password must be at least 8 characters", Toast.LENGTH_SHORT)
+            .show()
+        return false
+    }
+    return true
+}
+
+fun getUserAttribute(registrationActivity: RegistrationActivity): HashMap<String, String> {
+    return hashMapOf(
+        "nameAndSurname" to (registrationActivity.findViewById(R.id.namesurname) as EditText).text.toString(),
+        "username" to (registrationActivity.findViewById(R.id.username) as EditText).text.toString(),
+        "email" to (registrationActivity.findViewById(R.id.email) as EditText).text.toString(),
+        "password" to (registrationActivity.findViewById(R.id.password) as EditText).text.toString(),
+        "city" to (registrationActivity.findViewById(R.id.city) as EditText).text.toString(),
+        "phoneNumber" to (registrationActivity.findViewById(R.id.telephonenumber) as EditText).text.toString()
+    )
 }
