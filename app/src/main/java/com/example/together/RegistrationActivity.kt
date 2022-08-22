0ad9d34eb2387169f1220bcf752c4938527fa7d6
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-08-22 18:23:02 +0200

    Passwords are no longer managed by the app.

--- app/src/main/java/com/example/together/RegistrationActivity.kt
+++ app/src/main/java/com/example/together/RegistrationActivity.kt
@@ -23,118 +23,97 @@ class RegistrationActivity : AppCompatActivity() {
         val signupButton: Button = findViewById(R.id.signupbtndone)
         signupButton.setOnClickListener {
 
-            //Validate input from user
-            if (passwordCheck(
-                    this@RegistrationActivity,
-                    findViewById(R.id.password),
-                    findViewById(R.id.repeatpassword)
-                )
-            )
-
-                when {
-                    TextUtils.isEmpty(
-                        findViewById<EditText>(R.id.email).text.toString().trim { it <= ' ' }) -> {
-                        Toast.makeText(
-                            this@RegistrationActivity,
-                            "Please enter email",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    TextUtils.isEmpty(
-                        findViewById<EditText>(R.id.password).text.toString()
-                            .trim { it <= ' ' }) -> {
-                        Toast.makeText(
-                            this@RegistrationActivity,
-                            "Please enter password",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    else -> {
-
-
-                        val email: String =
-                            findViewById<EditText>(R.id.email).text.toString().trim { it <= ' ' }
-                        val password: String =
-                            findViewById<EditText>(R.id.password).text.toString().trim { it <= ' ' }
-
-                        //Sign-up user with email and password
-                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(this)
-                            { task ->
-                                //If the registration is successfully done
-                                if (task.isSuccessful) {
-
-                                    //firebase registered user
-                                    val firebaseUser: FirebaseUser = task.result!!.user!!
-
-                                    Toast.makeText(
-                                        this@RegistrationActivity,
-                                        "You are registered successfully",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                    Log.d(tag, "createUserWithEmail:success")
-
-                                    //Now we insert the user created in the Firestore Database
-                                    //prepare the user instance
-                                    val user = getUserAttribute(this@RegistrationActivity)
-
-                                    val db = Firebase.firestore
-
-                                    // Add a new document with the same ID of the firebase user ID
-                                    db.collection("users").document(firebaseUser.uid).set(user)
-                                        .addOnSuccessListener {
-                                            Log.d(tag, "DocumentSnapshot successfully written!")
-                                        }
-                                        .addOnFailureListener { e ->
-                                            Log.w(tag, "Error adding document", e)
-                                        }
-
-                                    //User is registered and so logged in, we send him to the homepage
-                                    val intent = Intent(
-                                        this@RegistrationActivity,
-                                        HomepageActivity::class.java
-                                    )
-                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    intent.putExtra("user_id", firebaseUser.uid)
-                                    intent.putExtra("email", email)
-                                    startActivity(intent)
-                                    finish()
-                                } else {
-                                    //If the registration was not successful, then show the error message
-                                    Toast.makeText(
-                                        this@RegistrationActivity,
-                                        task.exception!!.message.toString(),
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                    Log.w(tag, "createUserWithEmail:failure", task.exception)
-
-                                }
-
-                            }
-
-                    }
+            when {
+                TextUtils.isEmpty(
+                    findViewById<EditText>(R.id.email).text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Please enter email",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+
+                TextUtils.isEmpty(
+                    findViewById<EditText>(R.id.password).text.toString()
+                        .trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Please enter password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                !passwordsAreEquals(this@RegistrationActivity) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Passwords are not equals",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+
+                    val email: String =
+                        findViewById<EditText>(R.id.email).text.toString().trim { it <= ' ' }
+                    val password: String =
+                        findViewById<EditText>(R.id.password).text.toString().trim { it <= ' ' }
+
+                    //Sign-up user with email and password
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this)
+                        { task ->
+                            //If the registration is successfully done
+                            if (task.isSuccessful) {
+
+                                //firebase registered user
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                Toast.makeText(
+                                    this@RegistrationActivity,
+                                    "You are registered successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                Log.d(tag, "createUserWithEmail:success")
+
+                                //Now we insert the user created in the Firestore Database
+                                //prepare the user instance
+                                val user = getUserAttribute(this@RegistrationActivity)
+
+                                val db = Firebase.firestore
+
+                                // Add a new document with the same ID of the firebase user ID
+                                db.collection("users").document(firebaseUser.uid).set(user)
+                                    .addOnSuccessListener {
+                                        Log.d(tag, "DocumentSnapshot successfully written!")
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Log.w(tag, "Error adding document", e)
+                                    }
+
+                                //User is registered and so logged in, we send him to the homepage
+                                val intent = Intent(
+                                    this@RegistrationActivity,
+                                    HomepageActivity::class.java
+                                )
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", firebaseUser.uid)
+                                intent.putExtra("email", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                //If the registration was not successful, then show the error message
+                                Toast.makeText(
+                                    this@RegistrationActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                Log.w(tag, "createUserWithEmail:failure", task.exception)
+                            }
+                        }
+                }
+            }
         }
     }
-}
-
-fun passwordCheck(
-    activity: RegistrationActivity,
-    password: EditText,
-    repeatPsw: EditText
-): Boolean {
-    if (password.text.toString() != repeatPsw.text.toString()) {
-        Toast.makeText(activity, "The passwords are not equals", Toast.LENGTH_SHORT).show()
-        return false
-    }
-    if (password.text.toString().length < 8) {
-        Toast.makeText(activity, "Password must be at least 8 characters", Toast.LENGTH_SHORT)
-            .show()
-        return false
-    }
-    return true
 }
 
 fun getUserAttribute(registrationActivity: RegistrationActivity): HashMap<String, String> {
@@ -142,8 +121,11 @@ fun getUserAttribute(registrationActivity: RegistrationActivity): HashMap<String
         "nameAndSurname" to (registrationActivity.findViewById(R.id.namesurname) as EditText).text.toString(),
         "username" to (registrationActivity.findViewById(R.id.username) as EditText).text.toString(),
         "email" to (registrationActivity.findViewById(R.id.email) as EditText).text.toString(),
-        "password" to (registrationActivity.findViewById(R.id.password) as EditText).text.toString(),
         "city" to (registrationActivity.findViewById(R.id.city) as EditText).text.toString(),
         "phoneNumber" to (registrationActivity.findViewById(R.id.telephonenumber) as EditText).text.toString()
     )
+}
+
+fun passwordsAreEquals (registrationActivity: RegistrationActivity): Boolean{
+    return registrationActivity.findViewById<EditText>(R.id.password).text.toString() == registrationActivity.findViewById<EditText>(R.id.repeatpassword).text.toString()
 }
