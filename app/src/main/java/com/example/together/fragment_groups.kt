e350863c53b84ce0920f49bd7f131f0c1c26a84d
Appolloni21 <alessandro.appolloni21@gmail.com>
2022-08-30 18:00:49 +0200

    groups fragment linked with toolbar and bottombar

--- app/src/main/java/com/example/together/fragment_groups.kt
+++ app/src/main/java/com/example/together/fragment_groups.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayout
 
@@ -27,7 +28,28 @@ class fragment_groups : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewPager = view.findViewById<View>(R.id.pager)
         val tabLayout : View? = view.findViewById(R.id.tab_layout)
-        tabLayout.setupViewPager(viewPager)
+        //tabLayout.setupViewPager(viewPager)
+
+        //Setup the toolbar
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.homepage_toolbar)
+
+        //Disabling search item
+        val searchItem = toolbar.menu.findItem(R.id.search)
+        searchItem.isVisible = false
+        //Disabling add post item
+        val addItem = toolbar.menu.findItem(R.id.add)
+        addItem.isVisible = false
+        //Disabling settings menu
+        val settingsItem = toolbar.menu.findItem(R.id.action_settings)
+        settingsItem.isVisible = false
+
+        //Handle actions of toolbar items
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                else -> false
+            }
+        }
     }
 
 
