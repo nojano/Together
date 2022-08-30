6c83122e9257eb286d93d2b96869cc0cb947bc1e
nickdiesse <Cr4iglist>
2022-08-30 17:45:27 +0200

    fragment groups

---
+++ app/src/main/java/com/example/together/fragment_groups.kt
@@ -0,0 +1,34 @@
+package com.example.together
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+
+
+class fragment_groups : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_groups, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewPager = view.findViewById<View>(R.id.pager)
+        val tabLayout : View? = view.findViewById(R.id.tab_layout)
+        tabLayout.setupViewPager(viewPager)
+    }
+
+
+    }
