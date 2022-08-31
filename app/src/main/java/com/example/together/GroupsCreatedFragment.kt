8cd1b1f1cb439f276d1e6fa0a4b16f6cfd2e0c66
nickdiesse <Cr4iglist>
2022-08-31 16:50:31 +0200

    fragment groups created and joined

--- app/src/main/java/com/example/together/GroupsCreatedFragment.kt
+++ app/src/main/java/com/example/together/GroupsCreatedFragment.kt
@@ -1,58 +1,77 @@
 package com.example.together
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.together.databinding.FragmentGroupsCreatedBinding
+import com.example.together.databinding.FragmentHomePageBinding
 
 
 class GroupsCreatedFragment : Fragment() {
 
-
     private var _binding: FragmentGroupsCreatedBinding? = null
     private val binding get() = _binding!!
+    private lateinit var recyclerViewCreatedGroups: RecyclerView
+
+    private var layoutmanager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecycleAdapterCreatedPosts.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setHasOptionsMenu(true)
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentGroupsCreatedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerViewCreatedGroups = binding.recyclerViewPostCreated
+
+        layoutmanager = LinearLayoutManager(this.context)
+        recyclerViewCreatedGroups.layoutManager = layoutmanager
+
+        adapter = RecycleAdapterCreatedPosts()
+        recyclerViewCreatedGroups.adapter = adapter
 
         //Setup the toolbar
         binding.toolbar.inflateMenu(R.menu.homepage_toolbar)
 
-        //Search item
-        val searchItem = binding.toolbar.menu.findItem(R.id.search)
-        searchItem.isVisible = true
-        //Disabling add post item
-        val addItem = binding.toolbar.menu.findItem(R.id.add)
-        addItem.isVisible = false
         //Disabling settings menu
-        val settingsItem = binding.toolbar.menu.findItem(R.id.action_settings)
-        settingsItem.isVisible = false
+        //val settingsItem = binding.toolbar.menu.findItem(R.id.action_settings)
+        //settingsItem.isVisible = false
 
         //Handle actions of toolbar items
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
-
+                R.id.action_settings -> {
+                    startActivity(Intent(activity, SettingsActivity::class.java))
+                    true
+                }
+                R.id.add -> {
+                    startActivity(Intent(activity, NewPostActivity::class.java))
+                    true
+                }
                 else -> false
             }
         }
 
-
-        fun onDestroyView() {
-            super.onDestroyView()
-            _binding = null
-        }
-
     }
-}
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
--- app/src/main/java/com/example/together/GroupsJoinedFragment.kt
+++ app/src/main/java/com/example/together/GroupsJoinedFragment.kt
@@ -1,57 +1,78 @@
 package com.example.together
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.together.databinding.FragmentGroupsCreatedBinding
 import com.example.together.databinding.FragmentGroupsJoinedBinding
-
+import com.example.together.databinding.FragmentHomePageBinding
 
 
 class GroupsJoinedFragment : Fragment() {
 
     private var _binding: FragmentGroupsJoinedBinding? = null
     private val binding get() = _binding!!
+    private lateinit var recyclerViewJoinedGroups: RecyclerView
+
+    private var layoutmanager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecycleAdapterJoinedPosts.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setHasOptionsMenu(true)
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentGroupsJoinedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerViewJoinedGroups = binding.recyclerViewPostJoined
+
+        layoutmanager = LinearLayoutManager(this.context)
+        recyclerViewJoinedGroups.layoutManager = layoutmanager
+
+        adapter = RecycleAdapterJoinedPosts()
+        recyclerViewJoinedGroups.adapter = adapter
 
         //Setup the toolbar
         binding.toolbar.inflateMenu(R.menu.homepage_toolbar)
 
-        //Search item
-        val searchItem = binding.toolbar.menu.findItem(R.id.search)
-        searchItem.isVisible = false
-        //Disabling add post item
-        val addItem = binding.toolbar.menu.findItem(R.id.add)
-        addItem.isVisible = false
         //Disabling settings menu
-        val settingsItem = binding.toolbar.menu.findItem(R.id.action_settings)
-        settingsItem.isVisible = false
+        //val settingsItem = binding.toolbar.menu.findItem(R.id.action_settings)
+        //settingsItem.isVisible = false
 
         //Handle actions of toolbar items
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
-
+                R.id.action_settings -> {
+                    startActivity(Intent(activity, SettingsActivity::class.java))
+                    true
+                }
+                R.id.add -> {
+                    startActivity(Intent(activity, NewPostActivity::class.java))
+                    true
+                }
                 else -> false
             }
         }
 
-
-        fun onDestroyView() {
-            super.onDestroyView()
-            _binding = null
-        }
-
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 
 }
