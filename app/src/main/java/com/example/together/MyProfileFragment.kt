d331c4c6003a260b2bf8a3d63254c1a990c2020a
Appolloni21 <alessandro.appolloni21@gmail.com>
2022-08-31 22:12:22 +0200

    My Profile get() test from database

--- app/src/main/java/com/example/together/MyProfileFragment.kt
+++ app/src/main/java/com/example/together/MyProfileFragment.kt
@@ -2,6 +2,7 @@ package com.example.together
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+private const val TAG = "MyProfileFragment"
+
 class MyProfileFragment : Fragment() {
     private var _binding: FragmentMyProfileBinding? = null
     private val binding get() = _binding!!
@@ -19,7 +22,7 @@ class MyProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
+        Log.d(TAG, "onCreate Called")
     }
 
     override fun onCreateView(
@@ -27,11 +30,12 @@ class MyProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMyProfileBinding.inflate(inflater, container, false)
+        Log.d(TAG, "onCreateView Called")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        Log.d(TAG, "onViewCreated Called")
         //Setup the toolbar
         binding.toolbar.inflateMenu(R.menu.homepage_toolbar)
 
@@ -48,10 +52,6 @@ class MyProfileFragment : Fragment() {
         //Handle actions of toolbar items
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
-                /*R.id.action_settings -> {
-                    startActivity(Intent(activity, SettingsActivity::class.java))
-                    true
-                }*/
                 else -> false
             }
         }
@@ -64,15 +64,7 @@ class MyProfileFragment : Fragment() {
             startActivity(intent)
         }
 
-        /*val userId = myUserProfile.userID
-        val db = Firebase.firestore
-        val docRef = db.collection("user").document(userId)
-        docRef.get().addOnSuccessListener { document ->
-            if(document != null){
-                binding.city.text = document.data?.getValue("city").toString()
-            }
-        }*/
-
+        //getUserInfo()
         val tvUserName = binding.name
         tvUserName.text = myUserProfile.name
         val tvUserDescription = binding.description
@@ -88,8 +80,47 @@ class MyProfileFragment : Fragment() {
     }
 
     override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView Called")
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+        //appgetUserInfo()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy Called")
+        super.onDestroy()
+    }
+
+    private fun getUserInfo(){
+        val userId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val db = Firebase.firestore
+        val docRef = db.collection("user").document(userId)
+        docRef.get().addOnSuccessListener { document ->
+            if(document != null){
+                binding.city.text = "City: " + document.data?.getValue("city").toString()
+            }
+        }
+    }
+
 
 }
